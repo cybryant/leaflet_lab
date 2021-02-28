1049e7e0b83b3d41b92e8adf36c25061e4f5d308
Cherie <50756236+cybryant@users.noreply.github.com>
2021-02-27 23:24:24 -0500

    Added Popups
    popups with country & percent urban added along with hover events

--- js/main.js
+++ js/main.js
@@ -42,39 +42,60 @@ function calcPropRadius(attValue) {
     return radius;
 }
 
-//function to add circle markers for point features to the map
-function createPropSymbols(data, map){
-    //set attribute to visualize with proportional symbols
-    var attribute = "60Perc_Urban";
-    
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng){
+     //set attribute to visualize with proportional symbols
+    var attribute = "60Perc_Urban";   
+
     //create marker options
-    var geojsonMarkerOptions = {
-        radius: 8,
+    var options = {
         fillColor: "#ff7800",
         color: "#000",
         weight: 1,
         opacity: 1,
         fillOpacity: 0.8
-    };
-
+    };  
     
+    //for each feature, determine the value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+    
+    //give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);    
+    
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);  
+    
+    //build popup content string to identify the country
+    var popupContent = "<p><b>Country: </b>" + feature.properties.Entity + "</p>";
+    
+    //add formatted popup string for percent urban
+    var year = attribute.split("P")[0]; 
+    popupContent += "<p><b>Urban Population in " + year + ": </b>" + feature.properties[attribute] + "%</p>";
+    
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius)
+    });
+    
+    //event listerners to open popup on hover
+    layer.on({
+        mouseover: function(){
+            this.openPopup();
+        },
+        mouseout: function(){
+            this.closePopup();
+        }
+    });
+    
+    //return the circle marker to the L/geoJson pointToLayer option
+    return layer;
+};
+    
+//Add circle markers for point features to the map
+function createPropSymbols(data, map){
     //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            //for each feature, determine the value for the selected attribute
-            var attValue = Number(feature.properties[attribute]);
-            
-            /*
-            //examine the attribute value to check for correctness
-            console.log(feature.properties, attValue)
-            */
-            
-            //give each feature's curcle marker a radisu based on its attribute value
-            geojsonMarkerOptions.radius = calcPropRadius(attValue);
-            
-            //create circle markers
-            return L.circleMarker(latlng, geojsonMarkerOptions);
-        }
+        pointToLayer: pointToLayer
     }).addTo(map);
 };
 
